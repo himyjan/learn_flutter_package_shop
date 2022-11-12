a42678e7b77866051845a217255b4c2d99087681
himyjan <51815522+himyjan@users.noreply.github.com>
2022-11-12 11:43:45 +0800

    try android studio

--- serverpod_app_flutter/lib/main.dart
+++ serverpod_app_flutter/lib/main.dart
@@ -1,142 +1,59 @@
-import 'package:serverpod_app_client/serverpod_app_client.dart';
-import 'package:flutter/material.dart';
-import 'package:serverpod_flutter/serverpod_flutter.dart';
-import 'package:flutter_web_plugins/url_strategy.dart';
-import 'package:go_router/go_router.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flex_color_scheme/flex_color_scheme.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_web_plugins/url_strategy.dart';
+// import 'package:go_router/go_router.dart';
+// import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+// import 'package:flutter_localizations/flutter_localizations.dart';
+// import 'package:flutter_riverpod/flutter_riverpod.dart';
+// import 'package:flex_color_scheme/flex_color_scheme.dart';
+// import 'package:shop/pages/MyHomePage/MyHomePage.dart';
 
-// Sets up a singleton client object that can be used to talk to the server from
-// anywhere in our app. The client is generated from your server code.
-// The client is set up to connect to a Serverpod running on a local server on
-// the default port. You will need to modify this to connect to staging or
-// production servers.
-var client = Client('http://localhost:8080/')
-  ..connectivityMonitor = FlutterConnectivityMonitor();
+// void main() {
+//   usePathUrlStrategy(); // https://docs.flutter.dev/development/ui/navigation/url-strategies
+//   runApp(const ProviderScope(child: MyApp()));
+// }
 
-void main() {
-  runApp(const MyApp());
-}
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp.router(
+//       routerConfig: _router,
+//       onGenerateTitle: (context) => AppLocalizations.of(context).title,
+//       // This is the theme of your application.
+//       //
+//       // Try running your application with "flutter run". You'll see the
+//       // application has a blue toolbar. Then, without quitting the app, try
+//       // changing the primarySwatch below to Colors.green and then invoke
+//       // "hot reload" (press "r" in the console where you ran "flutter run",
+//       // or simply save your changes to "hot reload" in a Flutter IDE).
+//       // Notice that the counter didn't reset back to zero; the application
+//       // is not restarted.
+//       theme: FlexThemeData.light(scheme: FlexScheme.bigStone),
+//       // The Mandy red, dark theme.
+//       darkTheme: FlexThemeData.dark(scheme: FlexScheme.bigStone),
+//       // Use dark or light theme based on system setting.
+//       themeMode: ThemeMode.system,
+//       localizationsDelegates: AppLocalizations.localizationsDelegates,
+//       supportedLocales: AppLocalizations.supportedLocales,
+//     );
+//   }
+// }
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Serverpod Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Serverpod Example'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  MyHomePageState createState() => MyHomePageState();
-}
-
-class MyHomePageState extends State<MyHomePage> {
-  // These fields hold the last result or error message that we've received from
-  // the server or null if no result exists yet.
-  String? _resultMessage;
-  String? _errorMessage;
-
-  final _textEditingController = TextEditingController();
-
-  // Calls the `hello` method of the `example` endpoint. Will set either the
-  // `_resultMessage` or `_errorMessage` field, depending on if the call
-  // is successful.
-  void _callHello() async {
-    try {
-      final result = await client.example.hello(_textEditingController.text);
-      setState(() {
-        _resultMessage = result;
-      });
-    } catch (e) {
-      setState(() {
-        _errorMessage = '$e';
-      });
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(bottom: 16.0),
-              child: TextField(
-                controller: _textEditingController,
-                decoration: const InputDecoration(
-                  hintText: 'Enter your name',
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(bottom: 16.0),
-              child: ElevatedButton(
-                onPressed: _callHello,
-                child: const Text('Send to Server'),
-              ),
-            ),
-            _ResultDisplay(
-              resultMessage: _resultMessage,
-              errorMessage: _errorMessage,
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-// _ResultDisplays shows the result of the call. Either the returned result from
-// the `example.hello` endpoint method or an error message.
-class _ResultDisplay extends StatelessWidget {
-  final String? resultMessage;
-  final String? errorMessage;
-
-  const _ResultDisplay({
-    this.resultMessage,
-    this.errorMessage,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    String text;
-    Color backgroundColor;
-    if (errorMessage != null) {
-      backgroundColor = Colors.red[300]!;
-      text = errorMessage!;
-    } else if (resultMessage != null) {
-      backgroundColor = Colors.green[300]!;
-      text = resultMessage!;
-    } else {
-      backgroundColor = Colors.grey[300]!;
-      text = 'No server response yet.';
-    }
-
-    return Container(
-      height: 50,
-      color: backgroundColor,
-      child: Center(
-        child: Text(text),
-      ),
-    );
-  }
-}
+// final GoRouter _router = GoRouter(
+//   routes: <GoRoute>[
+//     GoRoute(
+//       path: '/',
+//       builder: (BuildContext context, GoRouterState state) {
+//         return const MyHomePage(title: 'Flutter Demo Home Page');
+//       },
+//     ),
+//     GoRoute(
+//       path: '/b',
+//       builder: (BuildContext context, GoRouterState state) {
+//         return const MyHomePage(title: 'Flutter Demo Home Page');
+//       },
+//     ),
+//   ],
+// );
